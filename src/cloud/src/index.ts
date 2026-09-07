d8c7772c3062d2076fb98e11bf462c5b1e920216
kolemasakar <kolemasakar@gmail.com>
2026-09-07 19:18:03 +0300

    R2: wire bounded Cobalt startup diagnostic

--- src/cloud/src/index.ts
+++ src/cloud/src/index.ts
@@ -2,6 +2,7 @@ import { loadConfig } from "./config.js";
 import { listen } from "./managed_server.js";
 import { configuredAssemblyAiSpeechModel } from "./stt_provider.js";
 import { resolveGeminiSttModel } from "./gemini_stt_provider.js";
+import { runCobaltStartupDiagnostic } from "./cobalt_startup_diagnostic.js";
 
 try {
   const config = loadConfig();
@@ -20,6 +21,20 @@ try {
       krc_media_stt_provider: config.krcMediaSttProvider ?? "assemblyai"
     })
   );
+
+  if (process.env.KRC_MEDIA_COBALT_DIAGNOSTIC_ONCE === "true") {
+    void runCobaltStartupDiagnostic(
+      config.cobaltEndpoint ?? null,
+      config.cobaltApiKey ?? null
+    ).then((diagnostic) => {
+      console.log(
+        JSON.stringify({
+          event: "cobalt_startup_diagnostic",
+          ...diagnostic
+        })
+      );
+    });
+  }
 
   const shutdown = (signal: string) => {
     console.log(
